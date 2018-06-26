ea17510d5bd765e6db152fcecaf7db9300eaa724
Sara Kim <sarakim0903@gmail.com>
2018-06-26 03:03:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,10 +21,11 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do
-    
-    
-    get '/square/:number' do
-    num = params[:number]
-    (num.to_i**2).to_s
+    num1 = params[:num1].to_i
+    num2 = params[:num2].to_i
+    (num1 * num2).to_s
   end
 end
+    
+    
+    
